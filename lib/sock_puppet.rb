8894217a85583d553c3b70a763f02f775d86c0c2
Gareth Rushgrove <gareth@morethanseven.net>
2013-04-07 16:58:04 +0100

    limit size of queue of reports to avoid deluge on first connect

--- lib/sock_puppet.rb
+++ lib/sock_puppet.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'eventmachine'
+require 'puppet'
 
 $:.push File.expand_path(__FILE__)
 
@@ -9,4 +10,5 @@ require 'sock_puppet/receive_server'
 require 'sock_puppet/send_server'
 
 class SockPuppet
+  MAX_QUEUE_SIZE = 5
 end
